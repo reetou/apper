33242d128e7bf26d8ad23d046bfb02231923b84a
reetou <fillmasterson@gmail.com>
2020-10-15 14:02:06 +0300

    Add load button

--- src/pages/Builder.tsx
+++ src/pages/Builder.tsx
@@ -4,15 +4,9 @@ import Rightbar from "../components/Rightbar";
 import styled from 'styled-components'
 import Simulator from "../components/Simulator";
 import BuilderContext from "../store/BuilderContext";
-import { CUSTOM_COMPONENT_TYPES } from "../components/mobile_components";
-import EditTextBlock from "../components/forms/EditTextBlock";
-import ReactModal from 'react-modal';
-import update from 'immutability-helper'
-import CustomGenericButton from "../components/mobile_components/CustomGenericButton";
 import BuilderModeToggle from "../components/BuilderModeToggle";
 import Navigation from "../components/Navigation";
 import EditTabbar from "../components/forms/EditTabbar";
-import CustomOnboarding from "../components/mobile_pages/CustomOnboarding";
 
 const Container = styled.div`
   display: flex;
@@ -20,7 +14,7 @@ const Container = styled.div`
 `
 
 export default function Builder() {
-  const { mode } = useContext(BuilderContext)
+  const { mode, pages, onboarding, tabbarSettings, tabbarEnabled } = useContext(BuilderContext)
   return (
     <Container>
       <Leftbar />
@@ -39,32 +33,22 @@ export default function Builder() {
           mode === 'edit_onboarding' ? <Simulator /> : null
         }
       </div>
+      <div
+        style={{ marginTop: 12 }}
+      >
+        <button onClick={() => {
+          navigator.clipboard.writeText(JSON.stringify({
+            project_name: 'Default',
+            tabbar_enabled: tabbarEnabled,
+            tabbar_settings: tabbarSettings,
+            onboarding,
+            pages,
+          }, null, 2))
+        }}>
+          Выгрузить
+        </button>
+      </div>
       <Rightbar/>
-      {/*<ReactModal*/}
-      {/*  isOpen={Boolean(editingComponent)}*/}
-      {/*  style={{*/}
-      {/*    content: {*/}
-      {/*      top: 200,*/}
-      {/*      left: 200,*/}
-      {/*      right: 200,*/}
-      {/*      bottom: 200,*/}
-      {/*    }*/}
-      {/*  }}*/}
-      {/*  onRequestClose={() => {*/}
-      {/*    setEditingComponent(undefined)*/}
-      {/*  }}*/}
-      {/*>*/}
-      {/*  <div>*/}
-      {/*    {editingComponent?.item_type === CUSTOM_COMPONENT_TYPES.TextBlock ? <EditTextBlock /> : null}*/}
-      {/*  </div>*/}
-      {/*  <button*/}
-      {/*    onClick={() => {*/}
-      {/*      setEditingComponent(undefined)*/}
-      {/*    }}*/}
-      {/*  >*/}
-      {/*    Применить*/}
-      {/*  </button>*/}
-      {/*</ReactModal>*/}
     </Container>
   )
 }
